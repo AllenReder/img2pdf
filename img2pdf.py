6a581a42ee84d00db7d695ec2070feaf3d7aaf51
Allen Yi <540809998@qq.com>
2023-11-06 14:09:20 +0800

    change arg name: imgs_path

--- img2pdf.py
+++ img2pdf.py
@@ -3,18 +3,18 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
 
-def img2pdf(image_path, output_name="output.pdf"):
+def img2pdf(imgs_path, output_name="output.pdf"):
     """
     Integrate pictures into a pdf file.
     Args:
-        image_path: A list of image paths.
+        imgs_path: A list of image paths.
         output_name: The name of the output pdf file.
     """
 
     output_pdf = output_name  # 指定生成的PDF文件名
     c = canvas.Canvas(output_pdf, pagesize=letter)  # 打开一个PDF文件，并设置页面大小
 
-    for image_file in image_path:
+    for image_file in imgs_path:
         print(f"正在处理图片文件 '{image_file}' ...")
         img = Image.open(image_file)  # 打开每个图片文件
         width, height = img.size  # 获取图片的宽度和高度
